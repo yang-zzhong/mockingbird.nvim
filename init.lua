9f8c4870b3465a5a13fa7098437437ffff1afa92
YangZhong <yang-zhong@udious.com>
2023-02-09 18:46:21 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,131 @@
+
+vim.opt.ts = 4
+vim.opt.sw = 4
+vim.opt.nu = true
+vim.opt.expandtab = true
+
+require('plugins')
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup({
+    create_in_closed_folder = true,
+    hijack_cursor = true,
+    open_on_setup = true,
+    open_on_setup_file = true,
+    focus_empty_on_setup = true,
+    sync_root_with_cwd = true,
+    view = {
+      adaptive_size = false,
+      mappings = {
+        list = {
+          { key = { "<2-RightMouse>", "<C-]>" }, action = "" }, -- cd
+          { key = "<C-v>", action = "" }, -- vsplit
+          { key = "<C-x>", action = "" }, -- split
+          { key = "<C-t>", action = "" }, -- tabnew
+          { key = "<BS>", action = "" }, -- close_node
+          { key = "<Tab>", action = "" }, -- preview
+          { key = "D", action = "" }, -- trash
+          { key = "[e", action = "" }, -- prev_diag_item
+          { key = "]e", action = "" }, -- next_diag_item
+          { key = "[c", action = "" }, -- prev_git_item
+          { key = "]c", action = "" }, -- next_git_item
+          { key = "-", action = "" }, -- dir_up
+          { key = "s", action = "" }, -- system_open
+          { key = "W", action = "" }, -- collapse_all
+          { key = "g?", action = "" }, -- toggle_help
+
+          { key = "d", action = "cd" }, -- remove
+          { key = "x", action = "remove" }, -- cut
+
+          { key = "t", action = "cut" },
+          { key = "<Space>p", action = "prev_diag_item" },
+          { key = "<Space>.", action = "next_diag_item" },
+          { key = "<Space>k", action = "prev_git_item" },
+          { key = "<Space>j", action = "next_git_item" },
+          { key = "u", action = "dir_up" },
+          { key = "'", action = "close_node" },
+          { key = '"', action = "collapse_all" },
+          { key = "?", action = "toggle_help" },
+        },
+      },
+    },
+    renderer = {
+      full_name = true,
+      group_empty = true,
+      special_files = {},
+      symlink_destination = false,
+      indent_markers = {
+        enable = true,
+      },
+      icons = {
+        git_placement = "signcolumn",
+        show = {
+          file = true,
+          folder = false,
+          folder_arrow = false,
+          git = true,
+        },
+      },
+    },
+    update_focused_file = {
+      enable = true,
+      update_root = true,
+      ignore_list = { "help" },
+    },
+    diagnostics = {
+      enable = true,
+      show_on_dirs = true,
+    },
+    filters = {
+      custom = {
+        "^.git$",
+      },
+    },
+    actions = {
+      change_dir = {
+        enable = false,
+        restrict_above_cwd = true,
+      },
+      open_file = {
+        resize_window = true,
+        window_picker = {
+          chars = "aoeui",
+        },
+      },
+      remove_file = {
+        close_window = false,
+      },
+    },
+    log = {
+      enable = false,
+      truncate = true,
+      types = {
+        all = false,
+        config = false,
+        copy_paste = false,
+        diagnostics = false,
+        git = false,
+        profile = false,
+        watcher = false,
+      },
+    },
+})
+
+
+-- Functional wrapper for mapping custom keybindings
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map("n", "<C-t>", ":NvimTreeToggle '<CR>", { silent = true })
---
+++ lua/plugins.lua
@@ -0,0 +1,22 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  -- nvim-tree
+  use {
+    "kyazdani42/nvim-web-devicons",
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  use { "fatih/vim-go" }
+  use {'neoclide/coc.nvim', branch = 'release'}
+
+end)
